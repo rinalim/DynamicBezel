3d95a00f81553726186740e82eed6ce6b7218166
rinalim <ooll00@naver.com>
2020-08-27 13:42:55 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ cp -f -r ./DynamicBezel /opt/retropie/configs/all/
 sudo chmod 755 /opt/retropie/configs/all/DynamicBezel/omxiv-bezel
 
 sudo sed -i '/DynamicBezel.py/d' /opt/retropie/configs/all/runcommand-onstart.sh
-echo '/usr/bin/python /opt/retropie/configs/all/DynamicBezel/DynamicBezel.py /dev/input/js0 &' >> /opt/retropie/configs/all/runcommand-onstart.sh
-echo '/usr/bin/python /opt/retropie/configs/all/DynamicBezel/DynamicBezel.py /dev/input/js1 &' >> /opt/retropie/configs/all/runcommand-onstart.sh
+echo 'sudo /usr/bin/python /opt/retropie/configs/all/DynamicBezel/DynamicBezel.py /dev/input/js0 &' >> /opt/retropie/configs/all/runcommand-onstart.sh
 sudo sed -i '/DynamicBezel.py/d' /opt/retropie/configs/all/runcommand-onend.sh
-echo 'pkill -ef DynamicBezel.py' >> /opt/retropie/configs/all/runcommand-onend.sh
+echo 'sudo pkill -ef DynamicBezel.py' >> /opt/retropie/configs/all/runcommand-onend.sh
