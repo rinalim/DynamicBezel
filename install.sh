84ac234db4edac5ae5e0dc4c1103285517bd79e9
rinalim <46272802+rinalim@users.noreply.github.com>
2021-01-02 02:24:31 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-sudo apt install python-pyudev python-bitstring -y
-sudo pip install keyboard==0.13.4
+sudo apt install python3-pyudev python3-bitstring -y
+sudo pip install keyboard
 
 rm -rf /opt/retropie/configs/all/DynamicBezel/
 mkdir /opt/retropie/configs/all/DynamicBezel/
@@ -8,7 +8,7 @@ cp -f -r ./DynamicBezel /opt/retropie/configs/all/
 sudo chmod 755 /opt/retropie/configs/all/DynamicBezel/omxiv-bezel
 
 sudo sed -i '/DynamicBezel.py/d' /opt/retropie/configs/all/runcommand-onstart.sh
-echo 'sudo /usr/bin/python /opt/retropie/configs/all/DynamicBezel/DynamicBezel.py /dev/input/js0 > /dev/null 2>&1 &' >> /opt/retropie/configs/all/runcommand-onstart.sh
+echo 'sudo /usr/bin/python3 /opt/retropie/configs/all/DynamicBezel/DynamicBezel.py /dev/input/js0 > /dev/null 2>&1 &' >> /opt/retropie/configs/all/runcommand-onstart.sh
 sudo sed -i '/DynamicBezel/d' /opt/retropie/configs/all/runcommand-onend.sh
 echo 'sudo pkill -ef DynamicBezel > /dev/null 2>&1' >> /opt/retropie/configs/all/runcommand-onend.sh
 sudo sed -i '/gpu_mem/d' /boot/config.txt
