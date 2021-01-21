f09a8ddeaddfd8600f2ec56a0b2f8a024b062ff1
rinalim <46272802+rinalim@users.noreply.github.com>
2021-01-21 16:57:18 +0900

    Update imagemagick-pi3.sh

--- imagemagick-pi3.sh
+++ imagemagick-pi3.sh
@@ -3,7 +3,7 @@ cd ~/src
 wget http://www.imagemagick.org/download/releases/ImageMagick-6.9.10-33.tar.xz
 sudo apt-get install build-essential checkinstall -y
 tar xf ImageMagick-6.9.10-33.tar.xz
-d ImageMagick-6.9.10-33/
+cd ImageMagick-6.9.10-33/
 ./configure
 make -j2
 sudo checkinstall
